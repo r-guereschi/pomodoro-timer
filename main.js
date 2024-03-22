4b91a4c6457ffb7ca56531c856859b71dab1b148
raphael guereschi <83614889+r-guereschi@users.noreply.github.com>
2024-03-22 10:50:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,30 @@ const html = document.querySelector("html");
 const botaoFoco = document.querySelector(".app__card-button--foco");
 const botaoCurto = document.querySelector(".app__card-button--curto");
 const botaoLongo = document.querySelector(".app__card-button--longo");
+const bannerFoto = document.querySelector(".app__image");
 
 botaoFoco.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "foco");
+  alterarContexto("foco");
+  botaoLongo.classList.remove("active");
+  botaoCurto.classList.remove("active");
+  botaoFoco.classList.add("active");
 });
 
 botaoCurto.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-curto");
+  alterarContexto("descanso-curto");
+  botaoLongo.classList.remove("active");
+  botaoCurto.classList.add("active");
+  botaoFoco.classList.remove("active");
 });
 
 botaoLongo.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-longo");
+  alterarContexto("descanso-longo");
+  botaoLongo.classList.add("active");
+  botaoCurto.classList.remove("active");
+  botaoFoco.classList.remove("active");
 });
+
+function alterarContexto(contexto) {
+  html.setAttribute("data-contexto", contexto);
+  bannerFoto.setAttribute("src", `/imagens/${contexto}.png`);
+}
