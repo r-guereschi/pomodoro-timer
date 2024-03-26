ce53c2c24c4c57d1559b17b959ad148ea1000ac7
raphael guereschi <83614889+r-guereschi@users.noreply.github.com>
2024-03-25 21:25:23 -0300

    botoes funcionando

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const botaoFoco = document.querySelector(".app__card-button--foco");
 const botaoCurto = document.querySelector(".app__card-button--curto");
 const botaoLongo = document.querySelector(".app__card-button--longo");
 const bannerFoto = document.querySelector(".app__image");
+const title = document.querySelector(".app__title");
+const titleStrong = document.querySelector(".app__title-strong");
+const botoes = document.querySelectorAll(".app__card-button");
 
 botaoFoco.addEventListener("click", () => {
   alterarContexto("foco");
@@ -28,4 +31,20 @@ botaoLongo.addEventListener("click", () => {
 function alterarContexto(contexto) {
   html.setAttribute("data-contexto", contexto);
   bannerFoto.setAttribute("src", `/imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      title.innerHTML = `Otimize sua produtividade,<br />
+      <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+      break;
+    case "descanso-curto":
+      title.innerHTML = `Que tal dar uma respirada? <br />
+      <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+      break;
+    case "descanso-longo":
+      title.innerHTML = `Hora de voltar à superfície. <br />
+      <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+      break;
+    default:
+      break;
+  }
 }
